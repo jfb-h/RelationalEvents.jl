2032e3963f60a8c30ec0a6caf9db6352e2f95203
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2023-06-28 10:08:39 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.36

---
+++ src/RelationalEvents.jl
@@ -0,0 +1,5 @@
+module RelationalEvents
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RelationalEvents
+using Test
+
+@testset "RelationalEvents.jl" begin
+    # Write your tests here.
+end
