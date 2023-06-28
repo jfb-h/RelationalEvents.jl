4ec01a1a2d965e5b6b4bce5406a65ba05ca7ce75
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2023-06-28 10:26:31 +0200

    initial commit

--- src/RelationalEvents.jl
+++ src/RelationalEvents.jl
@@ -1,5 +1,31 @@
 module RelationalEvents
 
-# Write your package code here.
+using Dates
 
+abstract type AbstractRelationalEvent end
+
+struct RelationalEvent{S,R,T}
+  sender::S
+  receiver::R
+  time::T
 end
+
+struct MarkedRelationalEvent{S,R,T,M}
+  sender::S
+  receiver::R
+  time::T
+  mark::M
+end
+
+struct History{E<:AbstractRelationalEvent,A}
+  events::Vector{E}
+  actors::Vector{A}
+end
+
+function active(h::History{E,A}, t)::Vector{A} where {E,A}
+end
+
+function active(h::History, a, t)::Bool
+end
+
+end # module
