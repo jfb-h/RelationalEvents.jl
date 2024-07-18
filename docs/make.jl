ef9f0480fc05d95e05ab6f7fac561398387f57f0
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2024-07-18 15:16:23 +0200

    use docs setup from PkgTemplates

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,26 @@
-using Documenter, RelationalEvents
+using RelationalEvents
+using Documenter
 
-push!(LOAD_PATH, "../src/")
+DocMeta.setdocmeta!(RelationalEvents, :DocTestSetup, :(using RelationalEvents); recursive=true)
 
-makedocs(
+makedocs(;
+    modules=[RelationalEvents],
+    authors="Jakob Hoffmann <jfb-hoffmann@hotmail.de> and contributors",
     sitename="RelationalEvents.jl",
+    format=Documenter.HTML(;
+        canonical="https://jfb-h.github.io/RelationalEvents.jl",
+        edit_link="main",
+        assets=String[],
+    ),
     pages=[
         "index.md",
         "Introduction" => "introduction.md",
         "Examples" => "examples.md",
         "Reference" => "api.md",
-    ]
+    ],
 )
 
-deploydocs(
-    repo="github.com/jfb-h/RelationalEvents.jl.git",
+deploydocs(;
+    repo="github.com/jfb-h/RelationalEvents.jl",
+    devbranch="main",
 )
